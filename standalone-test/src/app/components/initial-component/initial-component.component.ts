a14e994304cf7b75495b52847409623b1debeafc
DanyloBozhagora <danylobozhagoraup@gmail.com>
2023-10-04 09:39:12 +0200

    .

--- standalone-test/src/app/components/initial-component/initial-component.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-initial-component',
-  templateUrl: './initial-component.component.html'
-})
-export class InitialComponentComponent {
-
-}
